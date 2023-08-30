197925a7bb10cbf4fb7d00169d3aed6a870fced6
이서현 <kathyleesh7@gmail.com>
2023-08-31 00:17:52 +0900

    chore(SolFamilyApplication.java) : 프로젝트 초기 생성
    spring boot 프로젝트 초기 생성
    
    Related to #1

---
+++ backend/solfamily/src/main/java/com/sol/solfamily/SolFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.sol.solfamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolFamilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolFamilyApplication.class, args);
+    }
+
+}
---
+++ backend/solfamily/src/test/java/com/sol/solfamily/SolFamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sol.solfamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolFamilyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
