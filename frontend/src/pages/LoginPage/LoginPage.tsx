3cea9a2859ba947a3e0d7526812ddcce2307738b
suhwa-lee <tnghk9611@kumoh.ac.kr>
2023-09-03 22:39:46 +0900

    chore(SignupPage.tsx): #3 - 로그인 페이지 라우팅
    /login 경로로 로그인 페이지를 라우팅 처리함
    
    Related to: #3

---
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import ContentsFrame from '../../common/components/ContentsFrame/ContentsFrame';
+import Pages from '../../common/constants/Pages';
+
+const LoginPage = () => {
+    return <ContentsFrame page={Pages.LOGIN}>d</ContentsFrame>;
+};
+
+export default LoginPage;
