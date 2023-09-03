1e4979226c614b293ecfe3be40faf20015c5f711
이서현 <kathyleesh7@gmail.com>
2023-09-03 20:41:17 +0900

    refactor(kms) : #5 - kms 관심사 분리
    코드의 가독성과 재사용성이 향상을 위해 KMS 설정과 서비스 로직을 독립적인 클래스로 분리
    
    - KmsConfig를 도입하여 AWS KMS 관련 빈의 초기화 및 설정을 처리
    - KmsService는 의존성을 주입받아 데이터 복호화의 비즈니스 로직만을 담당하도록 수정
    
    Related to: #5

--- backend/authentication-service/src/main/java/com/authenticationservice/kms/KmsConfig.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/kms/KmsConfig.java
@@ -1,4 +1,4 @@
-package com.authenticationservice.global.kms;
+package com.authenticationservice.kms;
 
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.kms.AWSKMS;
--- backend/authentication-service/src/main/java/com/authenticationservice/kms/KmsService.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/kms/KmsService.java
@@ -1,11 +1,10 @@
-package com.authenticationservice.global.kms;
+package com.authenticationservice.kms;
 
 import com.amazonaws.services.kms.AWSKMS;
 import com.amazonaws.services.kms.model.DecryptRequest;
 import com.amazonaws.services.kms.model.EncryptionAlgorithmSpec;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.codec.binary.Base64;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.nio.ByteBuffer;
@@ -14,9 +13,9 @@ import java.nio.ByteBuffer;
 @RequiredArgsConstructor
 public class KmsService {
 
-    private AWSKMS kmsClient;
-    private String keyId;
-    private String encryptionAlgorithm;
+    private final AWSKMS kmsClient;
+    private final String keyId;
+    private final String encryptionAlgorithm;
 
     public String decryptData(String encryptedData) {
         try {
