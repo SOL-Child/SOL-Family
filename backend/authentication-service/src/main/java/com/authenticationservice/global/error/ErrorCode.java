10166aee30a61fc832d5e1d9506c1eb94c7e64d2
이서현 <kathyleesh7@gmail.com>
2023-09-09 02:36:01 +0900

    fix(TokenServiceImpl) : #28 - 예외 처리 변경
    - 토큰 생성과 조회 시 발생하는 예외 처리 변경
    
    Related to: #28

--- backend/authentication-service/src/main/java/com/authenticationservice/global/error/ErrorCode.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/global/error/ErrorCode.java
@@ -10,6 +10,9 @@ public enum ErrorCode {
     USER_NOT_EXIST(HttpStatus.BAD_REQUEST, "U-001", "회원이 존재하지 않습니다."),
     INVALID_PASSWORD_CHECK(HttpStatus.BAD_REQUEST, "U-002", "비밀번호가 일치하지 않습니다."),
 
+    // 토큰
+    UNEXPECTED_TOKEN(HttpStatus.BAD_REQUEST, "T-001", "만료되었거나 존재하지 않는 토큰입니다."),
+
     ;
 
 
