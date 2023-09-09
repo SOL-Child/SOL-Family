aed36dedf1680eea527264764e71138afa23d9f0
suhwa-lee <tnghk9611@kumoh.ac.kr>
2023-09-09 18:52:12 +0900

    feat(Header.tsx) : #13 - 헤더 기능 수정
    헤더 이미지 클릭 시 메인화면 이동하도록 수정
    
    Related to: #13

--- frontend/src/common/components/Header/Header.tsx
+++ frontend/src/common/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import styles from './Header.module.css';
 import logoSrc from '../../images/SF_full_logo.png';
+import { useNavigate } from 'react-router-dom';
 
 const Header = (data: any) => {
+    const navigate = useNavigate();
+
     return (
         <div className={styles.header}>
             <div>
-                <img src={logoSrc} alt="로고 이미지" />
+                <img src={logoSrc} alt="로고 이미지" onClick={() => navigate('/')} />
             </div>
         </div>
     );
