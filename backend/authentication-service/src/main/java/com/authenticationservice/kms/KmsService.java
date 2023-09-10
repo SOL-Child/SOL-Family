dcc170eeb0f60d04e9bc82b60ff8289d8bf26e55
이서현 <kathyleesh7@gmail.com>
2023-09-10 23:59:37 +0900

    fix(kms) : #34 - kms 복구화
    kms 다시 복구
    
    Related to: #34

--- backend/authentication-service/src/main/java/com/authenticationservice/kms/KmsService.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/kms/KmsService.java
@@ -1,37 +1,37 @@
-//package com.authenticationservice.kms;
-//
-//import com.amazonaws.services.kms.AWSKMS;
-//import com.amazonaws.services.kms.model.DecryptRequest;
-//import lombok.RequiredArgsConstructor;
-//import org.apache.commons.codec.binary.Base64;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.stereotype.Service;
-//
-//import java.nio.ByteBuffer;
-//
-//@Service
-//public class KmsService {
-//
-//    private final AWSKMS kmsClient;
-//    private final String keyId;
-//
-//    @Autowired
-//    public KmsService(AWSKMS kmsClient, @Qualifier("keyId") String keyId) {
-//        this.kmsClient = kmsClient;
-//        this.keyId = keyId;
-//    }
-//
-//    public String decryptData(String encryptedData) {
-//        try {
-//            DecryptRequest request = new DecryptRequest();
-//            request.withCiphertextBlob(ByteBuffer.wrap(Base64.decodeBase64(encryptedData)));
-//            request.withKeyId(keyId);
-//            ByteBuffer plainText = kmsClient.decrypt(request).getPlaintext();
-//            return new String(plainText.array());
-//        } catch (Exception e) {
-//            System.out.println("decrypt fail: " + e.getMessage());
-//        }
-//        return null;
-//    }
-//}
+package com.authenticationservice.kms;
+
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.model.DecryptRequest;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.nio.ByteBuffer;
+
+@Service
+public class KmsService {
+
+   private final AWSKMS kmsClient;
+   private final String keyId;
+
+   @Autowired
+   public KmsService(AWSKMS kmsClient, @Qualifier("keyId") String keyId) {
+       this.kmsClient = kmsClient;
+       this.keyId = keyId;
+   }
+
+   public String decryptData(String encryptedData) {
+       try {
+           DecryptRequest request = new DecryptRequest();
+           request.withCiphertextBlob(ByteBuffer.wrap(Base64.decodeBase64(encryptedData)));
+           request.withKeyId(keyId);
+           ByteBuffer plainText = kmsClient.decrypt(request).getPlaintext();
+           return new String(plainText.array());
+       } catch (Exception e) {
+           System.out.println("decrypt fail: " + e.getMessage());
+       }
+       return null;
+   }
+}
