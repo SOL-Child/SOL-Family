be47d75b3aa8c6c47dbef4ec8c874a04d98f1ecf
이서현 <kathyleesh7@gmail.com>
2023-09-10 18:47:16 +0900

    fix(KmsService) : #34 - 직접 생성자를 정의
    @Qualifier(keyId) String keyId로 직접 생성자를 주입
    
    Related to: #34

--- backend/authentication-service/src/main/java/com/authenticationservice/kms/KmsService.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/kms/KmsService.java
@@ -4,16 +4,23 @@ import com.amazonaws.services.kms.AWSKMS;
 import com.amazonaws.services.kms.model.DecryptRequest;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.nio.ByteBuffer;
 
 @Service
-@RequiredArgsConstructor
 public class KmsService {
 
     private final AWSKMS kmsClient;
     private final String keyId;
+
+    @Autowired
+    public KmsService(AWSKMS kmsClient, @Qualifier("keyId") String keyId) {
+        this.kmsClient = kmsClient;
+        this.keyId = keyId;
+    }
 
     public String decryptData(String encryptedData) {
         try {
