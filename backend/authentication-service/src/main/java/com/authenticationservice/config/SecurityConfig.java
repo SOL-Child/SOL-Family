c2123749b027f2378e2bd179211c9cc6e6ea7ae2
이서현 <kathyleesh7@gmail.com>
2023-09-14 04:10:02 +0900

    fix(SecurityConfig) : #34 - cors 추가
    http://3.35.49.187:8000, http://ec2-3-35-49-187.ap-northeast-2.compute.amazonaws.com:8000
    
    Related to: #34

--- backend/authentication-service/src/main/java/com/authenticationservice/config/SecurityConfig.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://solfamily-shinhan.com", "https://solfamily-shinhan.com", "http://15.164.212.241:8000", "https://15.164.212.241:8000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://solfamily-shinhan.com", "https://solfamily-shinhan.com", "http://3.35.49.187:8000", "http://ec2-3-35-49-187.ap-northeast-2.compute.amazonaws.com:8000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type", "x-auth-token", "X-CSRF-TOKEN"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
