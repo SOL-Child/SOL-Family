97d61088f6e1b8152b42bf8171c9e0b809e5f3d0
syhan7516 <syhan7516@naver.com>
2023-09-15 06:55:56 +0900

    feat(LoanController) : #60 - 대출 조회 api 생성
    대출 조회 성공 시 해당 대출 정보 데이터 반환
    
    Related to: #60

--- backend/account-service/src/main/java/com/accountservice/loan/LoanController.java
+++ backend/account-service/src/main/java/com/accountservice/loan/LoanController.java
@@ -1,6 +1,7 @@
 package com.accountservice.loan;
 import com.accountservice.global.dto.BaseResponseBody;
 import com.accountservice.loan.dto.request.LoanReqDto;
+import com.accountservice.loan.dto.response.LoanResDto;
 import com.accountservice.loan.service.LoanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +21,13 @@ public class LoanController {
                                                                  @RequestHeader(value = "X-JWT-Claim-auth", required = false) String auth,
                                                                  @RequestBody LoanReqDto loanReqDto) {
         loanService.createLoan(loanReqDto, auth, identification);
-        return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponseBody.of(0, "Success"));
+        return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponseBody.of(0,"Success"));
+    }
+
+    // 대출 조회
+    @GetMapping("/loans/{loanId}")
+    public ResponseEntity<? extends BaseResponseBody> getLoan(@PathVariable(name = "loanId") Long loanId) {
+        LoanResDto loan = loanService.findLoan(loanId);
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponseBody.of(0, loan));
     }
 }
