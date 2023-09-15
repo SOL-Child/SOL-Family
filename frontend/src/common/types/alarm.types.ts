9e8f7c120b8aa9f5c118df8fd84048270e8aecd1
suhwa-lee <tnghk9611@kumoh.ac.kr>
2023-09-16 01:37:22 +0900

    feat(AlarmPage.tsx) : #69 - 알람 페이지 구현
    대출 요청 알람 및 그 외 알람들을 확인하는 화면 구현
    
    Related to: #69

---
+++ frontend/src/common/types/alarm.types.ts
@@ -0,0 +1,10 @@
+// 대출 요청 알람 정보
+export interface AlarmInfo {
+    account: string;
+    identification: string;
+    requestDate: string;
+    purpose: string;
+    requestMoney: string;
+    solution: string;
+    state: string;
+}
