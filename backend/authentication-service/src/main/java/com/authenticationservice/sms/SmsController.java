266c9ddfa005632868488899b560539f4e474aac
이서현 <kathyleesh7@gmail.com>
2023-09-15 10:12:25 +0900

    fix(SmsController) : #61 - 전화번호 인증 시 중복 확인
    전화번호 인증 번호 요청시 중복 확인 후 진행
    
    Related to: #61

--- backend/authentication-service/src/main/java/com/authenticationservice/sms/SmsController.java
+++ backend/authentication-service/src/main/java/com/authenticationservice/sms/SmsController.java
@@ -2,9 +2,12 @@ package com.authenticationservice.sms;
 
 
 import com.authenticationservice.global.dto.BaseResponseBody;
+import com.authenticationservice.global.error.ErrorCode;
 import com.authenticationservice.sms.dto.request.SmsReqDto;
 import com.authenticationservice.sms.dto.request.SmsSendReqDto;
+import com.authenticationservice.user.exception.DuplicatePhoneException;
 import com.authenticationservice.sms.service.SmsService;
+import com.authenticationservice.user.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,11 +23,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "3. SMS API", description = "전화번호 인증 api")
 public class SmsController {
     private final SmsService smsService;
+    private final UserService userService;
 
     // 인증번호 발송
     @PostMapping("/sms/sends")
     public ResponseEntity<? extends BaseResponseBody> sendSms(@RequestBody SmsSendReqDto smsSendReqDto) {
-        System.out.println(smsSendReqDto.getPhone());
+        if(userService.findNewByPhone(smsSendReqDto.getPhone()).isPresent())
+            throw new DuplicatePhoneException(ErrorCode.DUPLICATE_PHONE_EXCEPTION);
         smsService.sendSms(smsSendReqDto.getPhone());
         return ResponseEntity.status(HttpStatus.CREATED).body(BaseResponseBody.of(0,"Success"));
     }
