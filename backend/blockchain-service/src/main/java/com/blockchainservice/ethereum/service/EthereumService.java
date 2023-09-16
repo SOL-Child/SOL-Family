3c6a7bce4fc90b0c55933f586746741e4cf31f04
이서현 <kathyleesh7@gmail.com>
2023-09-16 13:02:14 +0900

    feat(EthereumService) : #75 - 계좌 간 이체
    fromPrivateKey, toAddress, amount으로 이체
    
    Related to: #75

--- backend/blockchain-service/src/main/java/com/blockchainservice/ethereum/service/EthereumService.java
+++ backend/blockchain-service/src/main/java/com/blockchainservice/ethereum/service/EthereumService.java
@@ -1,5 +1,9 @@
 package com.blockchainservice.ethereum.service;
 
+import java.math.BigInteger;
+
 public interface EthereumService {
     String createAccount(String identification, String realAccount) throws Exception;
+
+    String transfer(String fromPrivateKey, String toAddress, BigInteger amount) throws Exception;
 }
--- backend/blockchain-service/src/main/java/com/blockchainservice/ethereum/service/EthereumServiceImpl.java
+++ backend/blockchain-service/src/main/java/com/blockchainservice/ethereum/service/EthereumServiceImpl.java
@@ -4,11 +4,18 @@ import com.blockchainservice.ethereum.entity.Ethereum;
 import com.blockchainservice.ethereum.repository.JpaEthereumRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.web3j.crypto.ECKeyPair;
-import org.web3j.crypto.Keys;
-import org.web3j.crypto.Wallet;
-import org.web3j.crypto.WalletFile;
+import org.web3j.crypto.*;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.RawTransactionManager;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.Transfer;
+import org.web3j.utils.Convert;
+
+import java.math.BigInteger;
 
 @Service("ethereumService")
 @Transactional
@@ -16,6 +23,12 @@ import org.web3j.crypto.WalletFile;
 public class EthereumServiceImpl extends Encrypt implements EthereumService {
 
     private final JpaEthereumRepository jpaEthereumRepository;
+
+
+    @Value("${infura.endpoint}")
+    private String infuraEndpoint;
+    private final Web3j web3j = Web3j.build(new HttpService(infuraEndpoint));
+
 
     @Override
     @Transactional
@@ -39,6 +52,17 @@ public class EthereumServiceImpl extends Encrypt implements EthereumService {
         return "0x" + wallet.getAddress();
     }
 
+    // 이체
+    @Override
+    @Transactional
+    public String transfer(String fromPrivateKey, String toAddress, BigInteger amount) throws Exception {
+        Credentials credentials = Credentials.create(fromPrivateKey);
+        TransactionManager transactionManager = new RawTransactionManager(web3j, credentials);
+        Transfer transfer = new Transfer(web3j, transactionManager);
 
+        // 이체 실행
+        TransactionReceipt transactionReceipt = transfer.sendFunds(toAddress, Convert.toWei(String.valueOf(amount), Convert.Unit.ETHER), Convert.Unit.WEI).send();
+        return transactionReceipt.getTransactionHash();
+    }
 
 }
