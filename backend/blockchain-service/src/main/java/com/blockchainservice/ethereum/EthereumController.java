7ae60dc5b53416091e82f33833bdc4e651c112f2
이서현 <kathyleesh7@gmail.com>
2023-09-16 16:30:37 +0900

    feat(EthereumService) : #78 - 거래내역 조회
    - 계좌 잔액 조회
    - 월간 거래 내역 조회
    - 월간 지출 내역 조회
    - 월간 수입 내역 조회
    
    Related to: #78

--- backend/blockchain-service/src/main/java/com/blockchainservice/ethereum/EthereumController.java
+++ backend/blockchain-service/src/main/java/com/blockchainservice/ethereum/EthereumController.java
@@ -5,8 +5,10 @@ import com.blockchainservice.ethereum.service.EthereumService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.web3j.protocol.core.methods.response.Transaction;
 
 import java.math.BigInteger;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,4 +40,40 @@ public class EthereumController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
+    // 계좌 잔액 조회
+    @GetMapping("/balance/{address}")
+    public ResponseEntity<BigInteger> getBalance(@PathVariable String address) {
+        try {
+            BigInteger balance = ethereumService.getBalance(address);
+            return ResponseEntity.ok(balance);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    // 월간 거래 내역 조회
+    @GetMapping("/month/{address}")
+    public ResponseEntity<List<Transaction>> getMonthBalance(@PathVariable String address,
+                                                        @RequestParam int year,
+                                                        @RequestParam int month) throws Exception {
+        return ResponseEntity.ok(ethereumService.getMonthlyTransactions(address, year, month));
+    }
+
+    // 월간 지출 내역 조회
+    @GetMapping("/expense/{address}")
+    public ResponseEntity<List<Transaction>> getExpense(@PathVariable String address,
+                                                        @RequestParam int year,
+                                                        @RequestParam int month) throws Exception {
+        return ResponseEntity.ok(ethereumService.getExpenseTransactions(address, year, month));
+    }
+
+    // 월간 수입 내역 조회
+    @GetMapping("/income/{address}")
+    public ResponseEntity<List<Transaction>> getIncome(@PathVariable String address,
+                                                        @RequestParam int year,
+                                                        @RequestParam int month) throws Exception {
+        return ResponseEntity.ok(ethereumService.getIncomeTransactions(address, year, month));
+    }
+
 }
