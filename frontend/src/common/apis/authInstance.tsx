6ca5ceaa5ece87ca9edb0386bc919ba00577dfba
suhwa-lee <tnghk9611@kumoh.ac.kr>
2023-09-16 12:02:26 +0900

    feat(authInstance.ts) : #80 - backend url https로 변경
    backend url을 http에 https로 변경
    
    Closed to: #80

--- frontend/src/common/apis/authInstance.tsx
+++ frontend/src/common/apis/authInstance.tsx
@@ -2,10 +2,10 @@ import axios, { AxiosInstance } from 'axios';
 
 // accessToken 필요한 instance
 const authInstance: AxiosInstance = axios.create({
-    // baseURL: 'https://www.solfamily-shinhan.com/v1',
-    baseURL: 'http://ec2-3-39-231-31.ap-northeast-2.compute.amazonaws.com:8080/v1',
+    baseURL: 'https://www.solfamily-shinhan.com',
     headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('SF_accessToken')}`,
     },
 });
 
