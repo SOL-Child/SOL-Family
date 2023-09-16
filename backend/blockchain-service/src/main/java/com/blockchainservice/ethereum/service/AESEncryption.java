784a551d7e9e50d7947e80b9a65f31ec92fc4259
이서현 <kathyleesh7@gmail.com>
2023-09-16 15:08:52 +0900

    fix(AESEncryption) : #77 - hashing -> AES 알고리즘 변경
    hashing의 단방향 문제를 해결하기 위해 알고리즘 변경
    
    Related to: #77

---
+++ backend/blockchain-service/src/main/java/com/blockchainservice/ethereum/service/AESEncryption.java
@@ -0,0 +1,50 @@
+package com.blockchainservice.ethereum.service;
+
+import com.blockchainservice.ethereum.exception.AESException;
+import com.blockchainservice.global.error.ErrorCode;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class AESEncryption {
+
+    @Value("${aes.key}")
+    private String KEY;
+
+    @Value("${aes.iv}")
+    private String INIT_VECTOR; // 16바이트 IV
+
+    public String encrypt(String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(INIT_VECTOR.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(KEY.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+            return Base64.getEncoder().encodeToString(encrypted);
+        } catch (Exception ex) {
+            throw new AESException(ErrorCode.ENCRYPT_ERROR);
+        }
+    }
+
+    public String decrypt(String encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(INIT_VECTOR.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(KEY.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            byte[] original = cipher.doFinal(Base64.getDecoder().decode(encrypted));
+
+            return new String(original);
+        } catch (Exception ex) {
+            throw new AESException(ErrorCode.DECRYPT_ERROR);
+        }
+    }
+}
